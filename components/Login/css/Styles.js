666a89ba6b35130128a8b8fb95470ed9c2fbc741
Jefferson <jeffcruz1998@hotmail.com>
2022-10-24 18:43:09 -0300

    24/10/2022

---
+++ components/Login/css/Styles.js
@@ -0,0 +1,84 @@
+import { StyleSheet } from "react-native";
+
+export const login = StyleSheet.create({
+	viewtela: {
+		padding:50,
+		backgroundColor: "#efefff"
+	},
+	viewlogo: {
+		alignItems:'center'
+	},
+	logo: {
+		width:200,
+		height:200
+	},
+	viewlogin: {
+		flexDirection: "row",
+		paddingTop: 70,
+	},
+	txtlogin: {
+		color: "#0c0031",
+		fontSize: 25,
+		fontWeight: 'bold',
+		textDecorationLine: 'underline',
+		marginLeft: "auto",
+		marginRight: "auto"
+	},
+	txtcadastro: {
+		color: "#0c0031",
+		fontSize: 25,
+		fontWeight: 'bold',
+		marginLeft: "auto",
+		marginRight: "auto"
+	},
+	btncadastro: {
+		color: "#0c0031",
+		fontSize: 25,
+		fontWeight: 'bold',
+		marginLeft: "auto",
+		marginRight: "auto"
+	},
+	safe:{
+		backgroundColor: 'white',
+    	width: '100%',
+    	borderRadius: 7,
+    	flexDirection: 'row',
+    	alignItems: 'center',
+    	borderWidth: 2,
+    	borderColor: 'black',
+	},
+	input1:{
+		paddingTop: 30,
+	},
+	inputlogin: {
+		borderWidth: 1,
+		height: 60,
+		backgroundColor: "white",
+		borderColor: "black",
+		borderRadius: 7,
+		padding:15,
+		color: "#acacac",
+		fontSize: 20
+	},
+	inputsenha: {
+		height: 60,
+		padding:15,
+		color: "#acacac",
+		fontSize: 20,
+		width: 320
+	},
+	btnentrar: {
+		width: 130,
+		height: 45,
+		marginRight: "auto",
+		marginLeft: "auto",
+		backgroundColor: "#1d0940",
+		borderRadius: 5,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	txtentrar: {
+		color: "white",
+		fontSize: 25,
+	}
+})
---
+++ components/Login/index.js
@@ -0,0 +1,57 @@
+import { TextInput, Image, View, Text, SafeAreaView, Pressable, TouchableOpacity, Button } from "react-native";
+import { login } from "./css/Styles";
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { useTogglePasswordVisibility } from './hooks/useTogglePasswordVisibility';
+import React, { useState } from 'react';
+
+export default function Login() {
+
+	const { passwordVisibility, rightIcon, handlePasswordVisibility } =
+    	useTogglePasswordVisibility();
+  	const [ password, setPassword ] = useState('');
+
+	return (
+		<View style={login.viewtela}>
+			<View style={login.viewlogo}>
+				<Image source={require("../../assets/logo.png")} style={login.logo}/>
+			</View>
+			<View style={login.viewlogin}>
+				<Text style={login.txtlogin}>LOGIN</Text>
+				<Pressable style={login.btncadastro} onPress={alert}>
+      				<Text style={login.txtcadastro}>CADASTRE-SE</Text>
+    			</Pressable>
+			</View>
+			<View style={login.input1}>
+				<TextInput
+        			style={login.inputlogin}
+        			placeholder="Usuario"
+        			keyboardType="email-address"/>
+			</View>
+			<View style={login.input1}>
+				<View style={login.safe}>
+					<TextInput
+          				style={login.inputsenha}
+         				name="Senha"
+          				placeholder="Senha"
+          				autoCapitalize="none"
+          				autoCorrect={false}
+          				textContentType="newPassword"
+          				secureTextEntry={passwordVisibility}
+          				value={password}
+          				enablesReturnKeyAutomatically
+          				onChangeText={text => setPassword(text)}/>
+						<Pressable onPress={handlePasswordVisibility}>
+          					<MaterialCommunityIcons name={rightIcon} size={26} color="#232323" />
+        				</Pressable>
+				</View>
+					<View style={login.input1}>
+						<Pressable style={login.btnentrar} onPress={alert}>
+      						<Text style={login.txtentrar}>Entrar</Text>
+    					</Pressable>
+					</View>
+			</View>
+		</View>
+	)
+}
