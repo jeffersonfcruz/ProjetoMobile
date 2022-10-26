8124ded76fbcf2e3e4b3e6fae72ed9b4ba007ac0
Jefferson <jeffcruz1998@hotmail.com>
2022-10-26 11:00:24 -0300

    26/10/2022

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { View } from 'react-native';
-import Home from './components';
-import Login from "./components/Login"
+import Home from './Screens/Home';
+import Login from './Screens/Login';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-		<Login/>
+		<Home/>
     </View>
   );
 }
